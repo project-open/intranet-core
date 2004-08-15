15f13222cdb2d89f39f2307c4135c8ccf5e1d334
Frank Bergmann <frank.bergmann@project-open.com>
2004-08-15 21:26:25 +0000

    - added upgrade scripts for core and ...

--- sql/oracle/upgrade/upgrade-2.0.0-2.0.1.sql
+++ sql/oracle/upgrade/upgrade-2.0.0-2.0.1.sql
@@ -1,12 +1,4 @@
-------------------------------------------------------------------------------
--- packages/intranet-core/sql/oracle/upgrade-2.0.0-2.0.1.sql
---
--- @author frank.bergmann@project-open.com
--- @creation-date 2004-08-01
---
 
-update im_view_columns set column_name='Company' where column_id=1;
+alter table im_view_columns add
+	extra_from		varchar(4000);
 
-@../intranet-customers.sql
-@../intranet-categories.sql
-@../intranet-views.sql
