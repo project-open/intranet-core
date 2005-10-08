9ac58c9e14fe96d454fcd86fb66460147236061c
Frank Bergmann <frank.bergmann@project-open.com>
2005-10-08 18:26:41 +0000

    - added /users/nuke-2.tcl functionality - now working more or less

--- www/users/nuke-2.tcl
+++ www/users/nuke-2.tcl
@@ -21,21 +21,26 @@ ad_page_contract {
     user_id:integer,notnull
 }
 
-# When there's a real orders table or system in place, 
-# n_orders should be set to the result of a query that determines
-# if the user has any orders
-set n_orders 0
+# ---------------------------------------------------------------
+# Defaults & Security
+# ---------------------------------------------------------------
 
-# Don't nuke anyone who pays us money ...
-if { $n_orders > 0 } {
-    ad_return_error "[_ intranet-core.lt_Cant_Nuke_a_Paying_Co]" "[_ intranet-core.lt_We_cant_nuke_a_paying]"
+set current_user_id [ad_maybe_redirect_for_registration]
+im_user_permissions $current_user_id $user_id view read write admin
+
+if {!$admin} {
+    ad_return_complaint "You need to have administration rights for this user."
     return
 }
 
-# have no mercy on the freeloaders
+
+# ---------------------------------------------------------------
+# Delete
+# ---------------------------------------------------------------
 
 # if this fails, it will probably be because the installation has 
 # added tables that reference the users table
+
 
 with_transaction {
     # education module
@@ -62,62 +67,213 @@ with_transaction {
     
 
     # bboard system
-    db_dml delete_user_bboard_email_alerts "delete from bboard_email_alerts where user_id = :user_id"
-    db_dml delete_user_bboard_thread_email_alerts "delete from bboard_thread_email_alerts where user_id = :user_id"
-    db_dml delete_user_bboard_unified "delete from bboard_unified where user_id = :user_id"
+    ns_log Notice "users/nuke2: bboard_email_alerts"
+    if {[db_table_exists bboard_email_alerts]} {
+	db_dml delete_user_bboard_email_alerts "delete from bboard_email_alerts where user_id = :user_id"
+	db_dml delete_user_bboard_thread_email_alerts "delete from bboard_thread_email_alerts where user_id = :user_id"
+	db_dml delete_user_bboard_unified "delete from bboard_unified where user_id = :user_id"
     
-    # deleting from bboard is hard because we have to delete not only a user's
-    # messages but also subtrees that refer to them
-    bboard_delete_messages_and_subtrees_where  -bind [list user_id $user_id] "user_id = :user_id"
+	# deleting from bboard is hard because we have to delete not only a user's
+	# messages but also subtrees that refer to them
+	bboard_delete_messages_and_subtrees_where  -bind [list user_id $user_id] "user_id = :user_id"
+    }
     
     # let's do the classifieds now
-    db_dml delete_user_classified_auction_bids "delete from classified_auction_bids where user_id = :user_id"
-    db_dml delete_user_classified_ads "delete from classified_ads where user_id = :user_id"
-    db_dml delete_user_classified_email_alerts "delete from classified_email_alerts where user_id = :user_id"
-    db_dml delete_user_neighbor_to_neighbor_comments "delete from general_comments 
- where on_which_table = 'neighbor_to_neighbor'
- and on_what_id in (select neighbor_to_neighbor_id 
-                   from neighbor_to_neighbor 
-                   where poster_user_id = :user_id)"
-    db_dml delete_user_neighbor_to_neighbor "delete from neighbor_to_neighbor where poster_user_id = :user_id"
+    ns_log Notice "users/nuke2: classified_auction_bids"
+    if {[db_table_exists classified_auction_bids]} {
+	db_dml delete_user_classified_auction_bids "delete from classified_auction_bids where user_id = :user_id"
+	db_dml delete_user_classified_ads "delete from classified_ads where user_id = :user_id"
+	db_dml delete_user_classified_email_alerts "delete from classified_email_alerts where user_id = :user_id"
+	db_dml delete_user_neighbor_to_neighbor_comments "
+	delete from general_comments 
+	where
+		on_which_table = 'neighbor_to_neighbor'
+		and on_what_id in (select neighbor_to_neighbor_id 
+	from neighbor_to_neighbor 
+	where poster_user_id = :user_id)"
+	db_dml delete_user_neighbor_to_neighbor "delete from neighbor_to_neighbor where poster_user_id = :user_id"
+    }
+
     # now the calendar
-    db_dml delete_user_calendar "delete from calendar where creation_user = :user_id"
+    ns_log Notice "users/nuke2: calendar"
+    if {[db_table_exists calendar]} {
+	db_dml delete_user_calendar "delete from calendar where creation_user = :user_id"
+    }
+
     # contest tables are going to be tough
-    set all_contest_entrants_tables [db_list unused "select entrants_table_name from contest_domains"]
-    foreach entrants_table $all_contest_entrants_tables {
-	db_dml delete_user_contest_entries "delete from $entrants_table where user_id = :user_id"
+    ns_log Notice "users/nuke2: entrants_table_name"
+    if {[db_table_exists entrants_table_name]} {
+	set all_contest_entrants_tables [db_list unused "select entrants_table_name from contest_domains"]
+	foreach entrants_table $all_contest_entrants_tables {
+	    db_dml delete_user_contest_entries "delete from $entrants_table where user_id = :user_id"
+	}
     }
 
     # spam history
-    db_dml delete_user_spam_history "delete from spam_history where creation_user = :user_id"
-    db_dml delete_user_spam_history_sent "update spam_history set last_user_id_sent = NULL
+    ns_log Notice "users/nuke2: spam_history"
+    if {[db_table_exists spam_history]} {
+	db_dml delete_user_spam_history "delete from spam_history where creation_user = :user_id"
+	db_dml delete_user_spam_history_sent "update spam_history set last_user_id_sent = NULL
                     where last_user_id_sent = :user_id"
+    }
 
     # calendar
-    db_dml delete_user_calendar_categories "delete from calendar_categories where user_id = :user_id"
+    ns_log Notice "users/nuke2: calendar_categories"
+    if {[db_table_exists calendar_categories]} {
+	db_dml delete_user_calendar_categories "delete from calendar_categories where user_id = :user_id"
+    }
 
     # sessions
-    db_dml delete_user_sec_sessions "delete from sec_sessions where user_id = :user_id"
-    db_dml delete_user_sec_login_tokens "delete from sec_login_tokens where user_id = :user_id"
+    ns_log Notice "users/nuke2: sec_sessions"
+    if {[db_table_exists sec_sessions]} {
+	db_dml delete_user_sec_sessions "delete from sec_sessions where user_id = :user_id"
+	db_dml delete_user_sec_login_tokens "delete from sec_login_tokens where user_id = :user_id"
+    }
     
     # general stuff
-    db_dml delete_user_general_comments "delete from general_comments where user_id = :user_id"
-    db_dml delete_user_comments "delete from comments where user_id = :user_id"
-    db_dml delete_user_links "delete from links where user_id = :user_id"
-    db_dml delete_user_chat_msgs "delete from chat_msgs where creation_user = :user_id"
-    db_dml delete_user_query_strings "delete from query_strings where user_id = :user_id"
-    db_dml delete_user_user_curriculum_map "delete from user_curriculum_map where user_id = :user_id"
-    db_dml delete_user_user_content_map "delete from user_content_map where user_id = :user_id"
-    db_dml delete_user_user_group_map "delete from user_group_map where user_id = :user_id"
+    ns_log Notice "users/nuke2: general_comments"
+    if {[db_table_exists general_comments]} {
+	db_dml delete_user_general_comments "delete from general_comments where user_id = :user_id"
+	db_dml delete_user_comments "delete from comments where user_id = :user_id"
+    }
+    ns_log Notice "users/nuke2: links"
+    if {[db_table_exists links]} {
+	db_dml delete_user_links "delete from links where user_id = :user_id"
+    }
+    ns_log Notice "users/nuke2: chat_msgs"
+    if {[db_table_exists chat_msgs]} {
+	db_dml delete_user_chat_msgs "delete from chat_msgs where creation_user = :user_id"
+    }
+    ns_log Notice "users/nuke2: query_strings"
+    if {[db_table_exists query_strings]} {
+	db_dml delete_user_query_strings "delete from query_strings where user_id = :user_id"
+    }
+    ns_log Notice "users/nuke2: user_curriculum_map"
+    if {[db_table_exists user_curriculum_map]} {
+	db_dml delete_user_user_curriculum_map "delete from user_curriculum_map where user_id = :user_id"
+    }
+    ns_log Notice "users/nuke2: user_content_map"
+    if {[db_table_exists user_content_map]} {
+	db_dml delete_user_user_content_map "delete from user_content_map where user_id = :user_id"
+    }
+    ns_log Notice "users/nuke2: user_group_map"
+    if {[db_table_exists user_group_map]} {
+	db_dml delete_user_user_group_map "delete from user_group_map where user_id = :user_id"
+    }
 
-    # core tables
-    db_dml delete_user_users_interests "delete from users_interests where user_id = :user_id"
-    db_dml delete_user_users_charges "delete from users_charges where user_id = :user_id"
-    db_dml set_referred_null_user_users_demographics "update users_demographics set referred_by = null where referred_by = :user_id"
-    db_dml delete_user_users_demographics "delete from users_demographics where user_id = :user_id"
-    db_dml delete_user_users_preferences "delete from users_preferences where user_id = :user_id"
-    db_dml delete_user_users_contact "delete from users_contact where user_id = :user_id"
+    ns_log Notice "users/nuke2: users_interests"
+    if {[db_table_exists users_interests]} {
+	db_dml delete_user_users_interests "delete from users_interests where user_id = :user_id"
+    }
+
+    ns_log Notice "users/nuke2: users_charges"
+    if {[db_table_exists users_charges]} {
+	db_dml delete_user_users_charges "delete from users_charges where user_id = :user_id"
+    }
+
+    ns_log Notice "users/nuke2: users_demographics"
+    if {[db_table_exists users_demographics]} {
+	db_dml set_referred_null_user_users_demographics "update users_demographics set referred_by = null where referred_by = :user_id"
+	db_dml delete_user_users_demographics "delete from users_demographics where user_id = :user_id"
+    }
+
+    ns_log Notice "users/nuke2: users_preferences"
+    if {[db_table_exists users_preferences]} {
+	db_dml delete_user_users_preferences "delete from users_preferences where user_id = :user_id"
+    }
+
+    if {[db_table_exists user_preferences]} {
+	db_dml delete_user_user_preferences "delete from user_preferences where user_id = :user_id"
+    }
+
+    if {[db_table_exists users_contact]} {
+	db_dml delete_user_users_contact "delete from users_contact where user_id = :user_id"
+    }
+
+    # Permissions
+    db_dml perms "delete from acs_permissions where grantee_id = :user_id"
+    db_dml perms "delete from acs_permissions where object_id = :user_id"
+
+
+    # Reassign objects to a default user...
+    set default_user 0
+    db_dml reassign_projects "update acs_objects set creation_user = :default_user where object_type = 'im_project' and creation_user = :user_id"
+    db_dml reassign_cr_revisions "update acs_objects set creation_user = :default_user where object_type = 'content_revision' and creation_user = :user_id"
+
+
+    # Deleting cost entries in acs_objects that are "dangeling", i.e. that don't have an
+    # entry in im_costs. These might have been created during manual deletion of objects
+    # Very dirty...
+    db_dml dangeling_costs "delete from acs_objects where object_type = 'im_cost' and object_id not in (select cost_id from im_costs)"
+
+    # Costs
+    set cost_infos [db_list_of_lists costs "select cost_id, object_type from im_costs, acs_objects where cost_id = object_id and (creation_user = :user_id or cause_object_id = :user_id)"]
+    foreach cost_info $cost_infos {
+	set cost_id [lindex $cost_info 0]
+	set object_type [lindex $cost_info 1]
+
+	ns_log Notice "users/nuke-2: deleting cost: ${object_type}__delete($cost_id)"
+	im_exec_dml del_cost "${object_type}__delete($cost_id)"
+    }
+
+
+    # Forum
+    db_dml forum "delete from im_forum_topic_user_map where user_id = :user_id"
+    db_dml forum "update im_forum_topics set owner_id = :default_user where owner_id = :user_id"
+    db_dml forum "update im_forum_topics set asignee_id = null where asignee_id = :user_id"
+    db_dml forum "update im_forum_topics set object_id = :default_user where object_id = :user_id"
+
+
+    # Timesheet
+    db_dml timesheet "delete from im_hours where user_id = :user_id"
+    db_dml timesheet "delete from im_user_absences where owner_id = :user_id"
+
+
+    # Remove user from business objects that we don't want to delete...
+    db_dml remove_from_companies "update im_companies set manager_id = null where manager_id = :user_id"
+    db_dml remove_from_projects "update im_projects set supervisor_id = null where supervisor_id = :user_id"
+    db_dml remove_from_projects "update im_projects set project_lead_id = null where project_lead_id = :user_id"
+
+    # Translation
+    db_dml trans_tasks "update im_trans_tasks set trans_id = null where trans_id = :user_id"
+    db_dml trans_tasks "update im_trans_tasks set edit_id = null where edit_id = :user_id"
+    db_dml trans_tasks "update im_trans_tasks set proof_id = null where proof_id = :user_id"
+    db_dml trans_tasks "update im_trans_tasks set other_id = null where other_id = :user_id"
+    db_dml task_actions "delete from im_task_actions where user_id = :user_id"
+
+    db_dml trans_quality "delete from im_trans_quality_entries where report_id in (
+	select report_id from im_trans_quality_reports where reviewer_id = :user_id
+    )"
+
+    db_dml trans_quality "delete from im_trans_quality_reports where reviewer_id = :user_id";
+
+
+    # Filestorage
+    db_dml forum "delete from im_fs_folder_status where user_id = :user_id"
+    db_dml filestorage "delete from im_fs_actions where user_id = :user_id"
+
+    set rels [db_list rels "select rel_id from acs_rels where object_id_one = :user_id or object_id_two = :user_id"]
+    foreach rel_id $rels {
+	db_dml del_rels "delete from group_element_index where rel_id = :rel_id"
+	db_dml del_rels "delete from im_biz_object_members where rel_id = :rel_id"
+	db_dml del_rels "delete from membership_rels where rel_id = :rel_id"
+	db_dml del_rels "delete from acs_rels where rel_id = :rel_id"
+	db_dml del_rels "delete from acs_objects where object_id = :rel_id"
+    }
+
+    db_dml party_approved_member_map "delete from party_approved_member_map where party_id = :user_id"
+    db_dml party_approved_member_map "delete from party_approved_member_map where member_id = :user_id"
+
+    if {[db_table_exists im_employees]} {
+	db_dml delete_employees "delete from im_employees where employee_id = :user_id"
+    }
+
+    ns_log Notice "users/nuke2: Main user tables"
     db_dml delete_user "delete from users where user_id = :user_id"
+    db_dml delete_user "delete from persons where person_id = :user_id"
+    db_dml delete_user "delete from parties where party_id = :user_id"
+    db_dml delete_user "delete from acs_objects where object_id = :user_id"
+
 } {
     
     set detailed_explanation ""
