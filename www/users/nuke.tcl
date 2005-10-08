2e6c069a13427cc337a732bf29be1bd5573a087a
Frank Bergmann <frank.bergmann@project-open.com>
2005-10-08 14:40:47 +0000

    - fixing /users/nuke

--- www/users/nuke.tcl
+++ www/users/nuke.tcl
@@ -23,43 +23,18 @@ ad_page_contract {
 }
 
 
-db_1row user_full_name "select first_names, last_name
-from users
-where user_id = :user_id"
+db_1row user_full_name "
+    select 
+	first_names, last_name
+    from
+	cc_users
+    where 
+	user_id = :user_id
+"
 
-db_release_unused_handles
+set page_title [_ intranet-core.lt_Nuke_first_names_last]
+set context_bar [im_context_bar [list /intranet/users/ "[_ intranet-core.Users]"] $page_title]
+
 
 set delete_user_link "<a href=\"delete?user_id=$user_id\">[_ intranet-core.lt_delete_this_user_inst]</a>"
 
-set page_content "[ad_admin_header "[_ intranet-core.lt_Nuke_first_names_last]"]
-
-<h2>[_ intranet-core.lt_Confirm_Nuking_first_]</h2>
-
-[ad_admin_context_bar [list "index.tcl" "[_ intranet-core.Users]"] [list "one.tcl?[export_url_vars user_id]" "[_ intranet-core.One_User]"] "[_ intranet-core.Nuke_user]"]
-
-<hr>
-
-[_ intranet-core.lt_Confirm_the_nuking_of]<a href=\"one?user_id=$user_id\">$first_names $last_name</a>
-
-<p>
-
-[_ intranet-core.lt_First_unless_first_na]
-
-<p>
-
-[_ intranet-core.lt_Nuking_is_a_violent_i]
-
-<P>
-
-<center>
-<form method=get action=nuke-2>
-<input type=hidden name=user_id value=\"$user_id\">
-<input type=submit value=\"[_ intranet-core.lt_Yes_Im_sure_that_I_wa]\">
-</form>
-</center>
-
-[ad_admin_footer]
-"
-
-
-doc_return  200 text/html $page_content
