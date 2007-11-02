d129a13cd88771751eea1111a1ed503c7aed278f
Frank Bergmann <frank.bergmann@project-open.com>
2007-11-02 16:21:25 +0000

    - added b3-3-patches branch to checkout

--- all-checkout.sh
+++ all-checkout.sh
@@ -1,143 +1,143 @@
 #!/bin/bash
 
-cvs checkout acs-admin
-cvs checkout acs-api-browser
-cvs checkout acs-authentication
-cvs checkout acs-automated-testing
-cvs checkout acs-bootstrap-installer
-cvs checkout acs-content-repository
-cvs checkout acs-core-docs
-cvs checkout acs-datetime
-cvs checkout acs-developer-support
-cvs checkout acs-events
-cvs checkout acs-kernel
-cvs checkout acs-lang
-cvs checkout acs-mail
-cvs checkout acs-mail-lite
-cvs checkout acs-messaging
-cvs checkout acs-reference
-cvs checkout acs-service-contract
-cvs checkout acs-subsite
-cvs checkout acs-tcl
-cvs checkout acs-templating
-cvs checkout acs-translations
-cvs checkout acs-workflow
+cvs checkout -r b3-3-patches acs-admin
+cvs checkout -r b3-3-patches acs-api-browser
+cvs checkout -r b3-3-patches acs-authentication
+cvs checkout -r b3-3-patches acs-automated-testing
+cvs checkout -r b3-3-patches acs-bootstrap-installer
+cvs checkout -r b3-3-patches acs-content-repository
+cvs checkout -r b3-3-patches acs-core-docs
+cvs checkout -r b3-3-patches acs-datetime
+cvs checkout -r b3-3-patches acs-developer-support
+cvs checkout -r b3-3-patches acs-events
+cvs checkout -r b3-3-patches acs-kernel
+cvs checkout -r b3-3-patches acs-lang
+cvs checkout -r b3-3-patches acs-mail
+cvs checkout -r b3-3-patches acs-mail-lite
+cvs checkout -r b3-3-patches acs-messaging
+cvs checkout -r b3-3-patches acs-reference
+cvs checkout -r b3-3-patches acs-service-contract
+cvs checkout -r b3-3-patches acs-subsite
+cvs checkout -r b3-3-patches acs-tcl
+cvs checkout -r b3-3-patches acs-templating
+cvs checkout -r b3-3-patches acs-translations
+cvs checkout -r b3-3-patches acs-workflow
 
-cvs checkout ajaxhelper
-cvs checkout ams
-cvs checkout batch-importer
-cvs checkout bug-tracker
-cvs checkout bulk-mail
-cvs checkout calendar
-cvs checkout categories
-cvs checkout contacts
-cvs checkout chat
-cvs checkout cms
-cvs checkout diagram
-cvs checkout ecommerce
-cvs checkout events
-cvs checkout general-comments
+cvs checkout -r b3-3-patches ajaxhelper
+cvs checkout -r b3-3-patches ams
+cvs checkout -r b3-3-patches batch-importer
+cvs checkout -r b3-3-patches bug-tracker
+cvs checkout -r b3-3-patches bulk-mail
+cvs checkout -r b3-3-patches calendar
+cvs checkout -r b3-3-patches categories
+cvs checkout -r b3-3-patches contacts
+cvs checkout -r b3-3-patches chat
+cvs checkout -r b3-3-patches cms
+cvs checkout -r b3-3-patches diagram
+cvs checkout -r b3-3-patches ecommerce
+cvs checkout -r b3-3-patches events
+cvs checkout -r b3-3-patches general-comments
 
-cvs checkout intranet-amberjack
-cvs checkout intranet-big-brother
-cvs checkout intranet-bug-tracker
-cvs checkout intranet-calendar
-cvs checkout intranet-calendar-holidays
-cvs checkout intranet-confdb
-cvs checkout intranet-core
-cvs checkout intranet-cost
-cvs checkout intranet-cost-center
-cvs checkout intranet-crm-tracking
+cvs checkout -r b3-3-patches intranet-amberjack
+cvs checkout -r b3-3-patches intranet-big-brother
+cvs checkout -r b3-3-patches intranet-bug-tracker
+cvs checkout -r b3-3-patches intranet-calendar
+cvs checkout -r b3-3-patches intranet-calendar-holidays
+cvs checkout -r b3-3-patches intranet-confdb
+cvs checkout -r b3-3-patches intranet-core
+cvs checkout -r b3-3-patches intranet-cost
+cvs checkout -r b3-3-patches intranet-cost-center
+cvs checkout -r b3-3-patches intranet-crm-tracking
 
-cvs checkout intranet-cust-baselkb
-cvs checkout intranet-cust-lexcelera
-cvs checkout intranet-cust-projop
+cvs checkout -r b3-3-patches intranet-cust-baselkb
+cvs checkout -r b3-3-patches intranet-cust-lexcelera
+cvs checkout -r b3-3-patches intranet-cust-projop
 
-cvs checkout intranet-cvs-integration
-cvs checkout intranet-dw-light
-cvs checkout intranet-dynfield
-cvs checkout intranet-exchange-rate
-cvs checkout intranet-expenses
-# cvs checkout intranet-expfind2-ora
-# cvs checkout intranet-expfind2-pg
-# cvs checkout intranet-expfind-ora
-# cvs checkout intranet-expfind-pg
-cvs checkout intranet-filestorage
-cvs checkout intranet-filestorage-openacs
-cvs checkout intranet-forum
-cvs checkout intranet-freelance
-cvs checkout intranet-freelance-invoices
-cvs checkout intranet-freelance-rfqs
-cvs checkout intranet-freelance-translation
-cvs checkout intranet-ganttproject
-cvs checkout intranet-hr
-cvs checkout intranet-pdf-htmldoc
-cvs checkout intranet-invoices
-cvs checkout intranet-invoices-templates
-cvs checkout intranet-mail-import
-cvs checkout intranet-material
-cvs checkout intranet-notes
-cvs checkout intranet-ophelia
-cvs checkout intranet-otp
-cvs checkout intranet-payments
-# cvs checkout intranet-planning
-# cvs checkout intranet-procedures
-cvs checkout intranet-release-mgmt
-cvs checkout intranet-reporting
-cvs checkout intranet-reporting-cubes
-cvs checkout intranet-reporting-dashboard
-cvs checkout intranet-reporting-finance
-cvs checkout intranet-reporting-translation
-cvs checkout intranet-reporting-tutorial
-# cvs checkout intranet-riskmanagement
-# cvs checkout intranet-search
-# cvs checkout intranet-search-ora
-# cvs checkout intranet-search-oracle
-cvs checkout intranet-search-pg
-cvs checkout intranet-search-pg-files
-# cvs checkout intranet-secure-login
-cvs checkout intranet-security-update-client
-# cvs checkout intranet-security-update-server
-cvs checkout intranet-simple-survey
-# cvs checkout intranet-spam
-# cvs checkout intranet-sql-selectors
-cvs checkout intranet-sysconfig
-# cvs checkout intranet-timesheet
-cvs checkout intranet-timesheet2
-cvs checkout intranet-timesheet2-invoices
-# cvs checkout intranet-timesheet2-task-popup
-cvs checkout intranet-timesheet2-tasks
-cvs checkout intranet-trans-invoices
-cvs checkout intranet-trans-project-wizard
-# cvs checkout intranet-trans-invoices-vaw
-cvs checkout intranet-translation
-cvs checkout intranet-trans-quality
-# cvs checkout intranet-travel
-cvs checkout intranet-update-client
-# cvs checkout intranet-update-server
-cvs checkout intranet-wiki
-cvs checkout intranet-workflow
-cvs checkout intranet-xmlrpc
+cvs checkout -r b3-3-patches intranet-cvs-integration
+cvs checkout -r b3-3-patches intranet-dw-light
+cvs checkout -r b3-3-patches intranet-dynfield
+cvs checkout -r b3-3-patches intranet-exchange-rate
+cvs checkout -r b3-3-patches intranet-expenses
+# cvs checkout -r b3-3-patches intranet-expfind2-ora
+# cvs checkout -r b3-3-patches intranet-expfind2-pg
+# cvs checkout -r b3-3-patches intranet-expfind-ora
+# cvs checkout -r b3-3-patches intranet-expfind-pg
+cvs checkout -r b3-3-patches intranet-filestorage
+cvs checkout -r b3-3-patches intranet-filestorage-openacs
+cvs checkout -r b3-3-patches intranet-forum
+cvs checkout -r b3-3-patches intranet-freelance
+cvs checkout -r b3-3-patches intranet-freelance-invoices
+cvs checkout -r b3-3-patches intranet-freelance-rfqs
+cvs checkout -r b3-3-patches intranet-freelance-translation
+cvs checkout -r b3-3-patches intranet-ganttproject
+cvs checkout -r b3-3-patches intranet-hr
+cvs checkout -r b3-3-patches intranet-pdf-htmldoc
+cvs checkout -r b3-3-patches intranet-invoices
+cvs checkout -r b3-3-patches intranet-invoices-templates
+cvs checkout -r b3-3-patches intranet-mail-import
+cvs checkout -r b3-3-patches intranet-material
+cvs checkout -r b3-3-patches intranet-notes
+cvs checkout -r b3-3-patches intranet-ophelia
+cvs checkout -r b3-3-patches intranet-otp
+cvs checkout -r b3-3-patches intranet-payments
+# cvs checkout -r b3-3-patches intranet-planning
+# cvs checkout -r b3-3-patches intranet-procedures
+cvs checkout -r b3-3-patches intranet-release-mgmt
+cvs checkout -r b3-3-patches intranet-reporting
+cvs checkout -r b3-3-patches intranet-reporting-cubes
+cvs checkout -r b3-3-patches intranet-reporting-dashboard
+cvs checkout -r b3-3-patches intranet-reporting-finance
+cvs checkout -r b3-3-patches intranet-reporting-translation
+cvs checkout -r b3-3-patches intranet-reporting-tutorial
+# cvs checkout -r b3-3-patches intranet-riskmanagement
+# cvs checkout -r b3-3-patches intranet-search
+# cvs checkout -r b3-3-patches intranet-search-ora
+# cvs checkout -r b3-3-patches intranet-search-oracle
+cvs checkout -r b3-3-patches intranet-search-pg
+cvs checkout -r b3-3-patches intranet-search-pg-files
+# cvs checkout -r b3-3-patches intranet-secure-login
+cvs checkout -r b3-3-patches intranet-security-update-client
+# cvs checkout -r b3-3-patches intranet-security-update-server
+cvs checkout -r b3-3-patches intranet-simple-survey
+# cvs checkout -r b3-3-patches intranet-spam
+# cvs checkout -r b3-3-patches intranet-sql-selectors
+cvs checkout -r b3-3-patches intranet-sysconfig
+# cvs checkout -r b3-3-patches intranet-timesheet
+cvs checkout -r b3-3-patches intranet-timesheet2
+cvs checkout -r b3-3-patches intranet-timesheet2-invoices
+# cvs checkout -r b3-3-patches intranet-timesheet2-task-popup
+cvs checkout -r b3-3-patches intranet-timesheet2-tasks
+cvs checkout -r b3-3-patches intranet-trans-invoices
+cvs checkout -r b3-3-patches intranet-trans-project-wizard
+# cvs checkout -r b3-3-patches intranet-trans-invoices-vaw
+cvs checkout -r b3-3-patches intranet-translation
+cvs checkout -r b3-3-patches intranet-trans-quality
+# cvs checkout -r b3-3-patches intranet-travel
+cvs checkout -r b3-3-patches intranet-update-client
+# cvs checkout -r b3-3-patches intranet-update-server
+cvs checkout -r b3-3-patches intranet-wiki
+cvs checkout -r b3-3-patches intranet-workflow
+cvs checkout -r b3-3-patches intranet-xmlrpc
 
-cvs checkout lars-blogger
-cvs checkout mail-tracking
-cvs checkout notifications
-cvs checkout organizations
-cvs checkout oryx-ts-extensions
-cvs checkout postal-address
+cvs checkout -r b3-3-patches lars-blogger
+cvs checkout -r b3-3-patches mail-tracking
+cvs checkout -r b3-3-patches notifications
+cvs checkout -r b3-3-patches organizations
+cvs checkout -r b3-3-patches oryx-ts-extensions
+cvs checkout -r b3-3-patches postal-address
 
-cvs checkout ref-countries
-cvs checkout ref-language
-cvs checkout ref-timezones
-cvs checkout ref-us-counties
-cvs checkout ref-us-states
-cvs checkout ref-us-zipcodes
+cvs checkout -r b3-3-patches ref-countries
+cvs checkout -r b3-3-patches ref-language
+cvs checkout -r b3-3-patches ref-timezones
+cvs checkout -r b3-3-patches ref-us-counties
+cvs checkout -r b3-3-patches ref-us-states
+cvs checkout -r b3-3-patches ref-us-zipcodes
 
-cvs checkout rss-support
-cvs checkout search
-cvs checkout simple-survey
-cvs checkout telecom-number
-cvs checkout trackback
-cvs checkout wiki
-cvs checkout workflow
-cvs checkout xml-rpc
+cvs checkout -r b3-3-patches rss-support
+cvs checkout -r b3-3-patches search
+cvs checkout -r b3-3-patches simple-survey
+cvs checkout -r b3-3-patches telecom-number
+cvs checkout -r b3-3-patches trackback
+cvs checkout -r b3-3-patches wiki
+cvs checkout -r b3-3-patches workflow
+cvs checkout -r b3-3-patches xml-rpc
