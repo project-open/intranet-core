d588a0cac6b9ed336ccbb7f8559b415c9864ead3
Frank Bergmann <frank.bergmann@project-open.com>
2008-05-26 14:17:09 +0000

    - fixed installer checkout script

--- all-installer-checkout.sh
+++ all-installer-checkout.sh
@@ -1,86 +1,86 @@
 #!/bin/bash
 
-cvs checkout -r b3-3-patches acs-admin
-cvs checkout -r b3-3-patches acs-api-browser
-cvs checkout -r b3-3-patches acs-authentication
-cvs checkout -r b3-3-patches acs-automated-testing
-cvs checkout -r b3-3-patches acs-bootstrap-installer
-cvs checkout -r b3-3-patches acs-content-repository
-cvs checkout -r b3-3-patches acs-core-docs
-cvs checkout -r b3-3-patches acs-datetime
-cvs checkout -r b3-3-patches acs-developer-support
-cvs checkout -r b3-3-patches acs-events
-cvs checkout -r b3-3-patches acs-kernel
-cvs checkout -r b3-3-patches acs-lang
-cvs checkout -r b3-3-patches acs-mail
-cvs checkout -r b3-3-patches acs-mail-lite
-cvs checkout -r b3-3-patches acs-messaging
-cvs checkout -r b3-3-patches acs-reference
-cvs checkout -r b3-3-patches acs-service-contract
-cvs checkout -r b3-3-patches acs-subsite
-cvs checkout -r b3-3-patches acs-tcl
-cvs checkout -r b3-3-patches acs-templating
-cvs checkout -r b3-3-patches acs-workflow
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-admin
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-api-browser
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-authentication
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-automated-testing
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-bootstrap-installer
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-content-repository
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-core-docs
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-datetime
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-developer-support
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-events
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-kernel
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-lang
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-mail
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-mail-lite
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-messaging
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-reference
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-service-contract
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-subsite
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-tcl
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-templating
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches acs-workflow
 
-cvs checkout -r b3-3-patches ajaxhelper
-cvs checkout -r b3-3-patches bug-tracker
-cvs checkout -r b3-3-patches bulk-mail
-cvs checkout -r b3-3-patches calendar
-cvs checkout -r b3-3-patches chat
-cvs checkout -r b3-3-patches cms
-cvs checkout -r b3-3-patches diagram
-cvs checkout -r b3-3-patches ecommerce
-cvs checkout -r b3-3-patches events
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches ajaxhelper
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches bug-tracker
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches bulk-mail
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches calendar
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches chat
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches cms
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches diagram
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches ecommerce
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches events
 
-cvs checkout -r b3-3-patches intranet-big-brother
-cvs checkout -r b3-3-patches intranet-bug-tracker
-cvs checkout -r b3-3-patches intranet-calendar
-cvs checkout -r b3-3-patches intranet-core
-cvs checkout -r b3-3-patches intranet-cost
-cvs checkout -r b3-3-patches intranet-cvs-integration
-cvs checkout -r b3-3-patches intranet-dw-light
-cvs checkout -r b3-3-patches intranet-dynfield
-cvs checkout -r b3-3-patches intranet-exchange-rate
-cvs checkout -r b3-3-patches intranet-expenses
-cvs checkout -r b3-3-patches intranet-filestorage
-cvs checkout -r b3-3-patches intranet-forum
-cvs checkout -r b3-3-patches intranet-ganttproject
-cvs checkout -r b3-3-patches intranet-hr
-cvs checkout -r b3-3-patches intranet-invoices
-cvs checkout -r b3-3-patches intranet-invoices-templates
-cvs checkout -r b3-3-patches intranet-mail-import
-cvs checkout -r b3-3-patches intranet-material
-cvs checkout -r b3-3-patches intranet-notes
-cvs checkout -r b3-3-patches intranet-payments
-cvs checkout -r b3-3-patches intranet-reporting
-cvs checkout -r b3-3-patches intranet-reporting-tutorial
-cvs checkout -r b3-3-patches intranet-search-pg
-cvs checkout -r b3-3-patches intranet-search-pg-files
-cvs checkout -r b3-3-patches intranet-security-update-client
-cvs checkout -r b3-3-patches intranet-simple-survey
-cvs checkout -r b3-3-patches intranet-sysconfig
-cvs checkout -r b3-3-patches intranet-timesheet2
-cvs checkout -r b3-3-patches intranet-timesheet2-invoices
-cvs checkout -r b3-3-patches intranet-timesheet2-task-popup
-cvs checkout -r b3-3-patches intranet-timesheet2-tasks
-cvs checkout -r b3-3-patches intranet-tinytm
-cvs checkout -r b3-3-patches intranet-trans-invoices
-cvs checkout -r b3-3-patches intranet-trans-project-wizard
-cvs checkout -r b3-3-patches intranet-translation
-cvs checkout -r b3-3-patches intranet-update-client
-cvs checkout -r b3-3-patches intranet-wiki
-cvs checkout -r b3-3-patches intranet-workflow
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-big-brother
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-bug-tracker
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-calendar
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-core
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-cost
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-cvs-integration
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-dw-light
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-dynfield
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-exchange-rate
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-expenses
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-filestorage
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-forum
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-ganttproject
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-hr
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-invoices
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-invoices-templates
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-mail-import
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-material
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-notes
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-payments
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-reporting
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-reporting-tutorial
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-search-pg
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-search-pg-files
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-security-update-client
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-simple-survey
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-sysconfig
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-timesheet2
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-timesheet2-invoices
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-timesheet2-task-popup
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-timesheet2-tasks
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-tinytm
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-trans-invoices
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-trans-project-wizard
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-translation
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-update-client
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-wiki
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches intranet-workflow
 
-cvs checkout -r b3-3-patches notifications
-cvs checkout -r b3-3-patches ref-countries
-cvs checkout -r b3-3-patches ref-language
-cvs checkout -r b3-3-patches ref-timezones
-cvs checkout -r b3-3-patches ref-us-countries
-cvs checkout -r b3-3-patches ref-us-states
-cvs checkout -r b3-3-patches ref-us-zipcodes
-cvs checkout -r b3-3-patches rss-support
-cvs checkout -r b3-3-patches search
-cvs checkout -r b3-3-patches simple-survey
-cvs checkout -r b3-3-patches wiki
-cvs checkout -r b3-3-patches workflow
-cvs checkout -r b3-3-patches xml-rpc
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches notifications
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches ref-countries
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches ref-language
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches ref-timezones
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches ref-us-countries
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches ref-us-states
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches ref-us-zipcodes
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches rss-support
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches search
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches simple-survey
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches wiki
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches workflow
+cvs -d :pserver:anonymous@berlin.dnsalias.com:/home/cvsroot checkout -r b3-3-patches xml-rpc
