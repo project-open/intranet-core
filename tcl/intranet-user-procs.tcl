df6e714a162b4153fa1e939f773bda2dc891d39e
Frank Bergmann <frank.bergmann@project-open.com>
2014-01-14 15:49:17 +0000

    - SL:   Fixed issue with wrong user_selection lists for timesheet   vs. absences

--- tcl/intranet-user-procs.tcl
+++ tcl/intranet-user-procs.tcl
@@ -328,7 +328,7 @@ ad_proc -public im_user_direct_reports_options_helper {
 }
 
 
-ad_proc im_user_timesheet_select {
+ad_proc im_user_timesheet_hours_select {
     {-include_empty_p 0}
     {-include_empty_name "All"}
     {-enable_groups_p 0}
@@ -338,23 +338,108 @@ ad_proc im_user_timesheet_select {
     Returns an html select box named $select_name and defaulted to
     $default with a list of all the available options
 } {
-    set user_options [im_user_timesheet_options -enable_groups_p $enable_groups_p]
+    set user_options [im_user_timesheet_hours_options -enable_groups_p $enable_groups_p]
     if {$include_empty_p} { set user_options [linsert $user_options 0 [list $include_empty_name ""]] }
     return [im_options_to_select_box $select_name $user_options $default]
 }
 
 
-ad_proc -public im_user_timesheet_options {
+ad_proc -public im_user_timesheet_hours_options {
     {-enable_groups_p 0}
 } {
     Returns the options for a drop-down box with users for the
-    absences and timesheet "log for user" pages.
+    timesheet "log for user" pages.
+} {
+    set current_user_id [ad_maybe_redirect_for_registration]
+    set name_order [parameter::get -package_id [apm_package_id_from_key intranet-core] -parameter "NameOrder" -default 1]
+
+    set view_hours_all_p [im_permission $current_user_id "view_hours_all"]
+    set add_hours_all_p [im_permission $current_user_id "add_hours_all"]
+    set add_hours_direct_reports_p [im_permission $current_user_id "add_hours_direct_reports"]
+
+    set all_user_options [im_user_options -include_empty_p 0 -group_name "Employees"]
+    set direct_reports_options [im_user_direct_reports_options -user_id $current_user_id]
+    set direct_report_ids [im_user_direct_reports_ids -user_id $current_user_id]
+
+    # all users into "direct reports" and "others" (=Employees - direct reports)
+    foreach t $direct_reports_options {
+	set uid [lindex $t 1]
+	set direct_reports_hash($uid) $uid
+    }
+
+    set other_options [list]
+    foreach t $all_user_options {
+	set uname [lindex $t 0]
+	set uid [lindex $t 1]
+	if {![info exists direct_reports_hash($uid)]} {
+	    lappend other_options $t
+	}
+	set direct_reports_hash($uid) $uid
+    }
+
+    # Start building the list of select options
+    # Show always "mine" 
+    set user_selection_options [list]
+    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Mine Mine] "mine"]
+
+    # Direct direct_reports 
+    if {$add_hours_direct_reports_p || $add_hours_all_p || $view_hours_all_p} { 
+	if {0 != [llength $direct_reports_options] } {
+	    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Direct_reports "Direct reports"] "direct_reports"]
+	    foreach t $direct_reports_options {
+		set uname [lindex $t 0]
+		set uid [lindex $t 1]
+		lappend user_selection_options [list "&nbsp;&nbsp;&nbsp;&nbsp;$uname" $uid]
+	    }
+	}
+    }
+
+    # All
+    if {$add_hours_all_p || $view_hours_all_p} {
+	lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.All "All"] "all"] 
+	foreach t $other_options { 
+	    set uname [lindex $t 0]
+	    set uid [lindex $t 1]
+	    lappend user_selection_options [list "&nbsp;&nbsp;&nbsp;&nbsp;$uname" $uid]
+	}
+	if {$enable_groups_p} {
+	    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Employees "Employees"] "employees"]
+	    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Providers "Providers"] "providers"]
+	    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Customers "Customers"] "customers"]
+	}
+    }
+    return $user_selection_options
+}
+
+
+
+
+ad_proc im_user_timesheet_absences_select {
+    {-include_empty_p 0}
+    {-include_empty_name "All"}
+    {-enable_groups_p 0}
+    select_name
+    { default "" }
+} {
+    Returns an html select box named $select_name and defaulted to
+    $default with a list of all the available options
+} {
+    set user_options [im_user_timesheet_absences_options -enable_groups_p $enable_groups_p]
+    if {$include_empty_p} { set user_options [linsert $user_options 0 [list $include_empty_name ""]] }
+    return [im_options_to_select_box $select_name $user_options $default]
+}
+
+
+ad_proc -public im_user_timesheet_absences_options {
+    {-enable_groups_p 0}
+} {
+    Returns the options for a drop-down box with users for the
+    absences and timesheet_absences_ "log for user" pages.
 } {
     set current_user_id [ad_maybe_redirect_for_registration]
     set view_absences_all_p [im_permission $current_user_id "view_absences_all"]
     set add_absences_all_p [im_permission $current_user_id "add_absences_all"]
     set view_absences_direct_reports_p [im_permission $current_user_id "view_absences_direct_reports"]
-    set add_hours_direct_reports_p [im_permission $current_user_id "add_hours_direct_reports"]
     set add_absences_direct_reports_p [im_permission $current_user_id "add_absences_direct_reports"]
     set name_order [parameter::get -package_id [apm_package_id_from_key intranet-core] -parameter "NameOrder" -default 1]
 
@@ -381,9 +466,8 @@ ad_proc -public im_user_timesheet_options {
     set user_selection_options [list]
     lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Mine Mine] "mine"]
 
-
     # Direct direct_reports 
-    if {$add_hours_direct_reports_p || $view_absences_direct_reports_p || $add_absences_all_p || $view_absences_all_p} { 
+    if {$view_absences_direct_reports_p || $add_absences_all_p || $view_absences_all_p} { 
 	if {0 != [llength $direct_reports_options] } {
 	    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Direct_reports "Direct reports"] "direct_reports"]
 	    foreach t $direct_reports_options {
@@ -402,16 +486,24 @@ ad_proc -public im_user_timesheet_options {
 	    set uid [lindex $t 1]
 	    lappend user_selection_options [list "&nbsp;&nbsp;&nbsp;&nbsp;$uname" $uid]
 	}
-
 	if {$enable_groups_p} {
 	    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Employees "Employees"] "employees"]
 	    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Providers "Providers"] "providers"]
 	    lappend user_selection_options [list [lang::message::lookup "" intranet-timesheet2.Customers "Customers"] "customers"]
 	}
     }
-
     return $user_selection_options
 }
+
+
+
+
+
+
+
+
+
+
 
 
 # *********************************************************
