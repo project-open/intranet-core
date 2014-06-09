dd42c2ed38d8e7ae8a828d9a12d85106418f5875
Frank Bergmann <frank.bergmann@project-open.com>
2014-06-09 09:15:48 +0000

    - Who's Online:   Fixed issue with apparently unretreivable user

--- www/whos-online.tcl
+++ www/whos-online.tcl
@@ -36,6 +36,7 @@ set not_shown 0
 foreach user_id [whos_online::user_ids] {
     acs_user::get -user_id $user_id -array user
     set first_request_minutes [expr [whos_online::seconds_since_first_request $user_id] / 60]
+    if {![info exists user(first_names)]} { continue }
     set user_name "$user(first_names) $user(last_name)"
     set user_url "/intranet/users/view?user_id=$user_id"
 
