37e43f3e9d00f95377b86a268e6b717c1c024daa
Frank Bergmann <frank.bergmann@project-open.com>
2016-02-19 10:14:25 +0000

    - Added default V5.0 checkout script

---
+++ all-v50-checkout.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+cvs checkout acs-admin
+cvs checkout acs-api-browser
+cvs checkout acs-authentication
+cvs checkout acs-automated-testing
+cvs checkout acs-bootstrap-installer
+cvs checkout acs-content-repository
+cvs checkout acs-core-docs
+cvs checkout acs-datetime
+cvs checkout acs-developer-support
+cvs checkout acs-events
+cvs checkout acs-kernel
+cvs checkout acs-lang
+# cvs checkout acs-lang-server				# (internal)
+cvs checkout acs-mail
+cvs checkout acs-mail-lite
+cvs checkout acs-messaging
+cvs checkout acs-reference
+cvs checkout acs-service-contract
+cvs checkout acs-subsite
+cvs checkout acs-tcl
+cvs checkout acs-templating
+cvs checkout acs-translations
+cvs checkout acs-workflow
+# cvs checkout auth-ldap
+cvs checkout auth-ldap-adldapsearch
+cvs checkout attachments
+cvs checkout ajaxhelper
+# cvs checkout batch-importer
+# cvs checkout bug-tracker
+# cvs checkout bulk-mail
+cvs checkout calendar
+cvs checkout categories
+# cvs checkout chat
+# cvs checkout cms
+cvs checkout diagram
+cvs checkout file-storage
+cvs checkout general-comments
+# cvs checkout intranet-amberjack
+# cvs checkout intranet-audit
+# cvs checkout intranet-asus-server			# (internal)
+# cvs checkout intranet-baseline
+# cvs checkout intranet-big-brother
+cvs checkout intranet-bug-tracker
+cvs checkout intranet-calendar
+# cvs checkout intranet-calendar-holidays		# (obsolete)
+cvs checkout intranet-confdb
+# cvs checkout intranet-contacts			# (obsolete)
+cvs checkout intranet-core
+cvs checkout intranet-cost
+# cvs checkout intranet-cost-center
+# cvs checkout intranet-crm-tracking
+# cvs checkout intranet-csv-import			# (work in progress)
+# cvs checkout intranet-cust-baselkb			# (customer package)
+# cvs checkout intranet-cust-cambridge			# (customer package)
+# cvs checkout intranet-cust-economedic			# (customer package)
+# cvs checkout intranet-cust-eicom			# (customer package)
+# cvs checkout intranet-cust-genedata			# (customer package)
+# cvs checkout intranet-cust-issa			# (customer package)
+# cvs checkout intranet-cust-lexcelera			# (customer package)
+# cvs checkout intranet-cust-moravia			# (customer package)
+# cvs checkout intranet-cust-processa			# (customer package)
+# cvs checkout intranet-cust-projop			# (customer package)
+# cvs checkout intranet-cust-reinisch			# (customer package)
+# cvs checkout intranet-cust-versia			# (customer package)
+# cvs checkout intranet-cust-whp			# (customer package)
+# cvs checkout intranet-cust-yada			# (customer package)
+cvs checkout intranet-cvs-integration
+cvs checkout intranet-dw-light
+cvs checkout intranet-dynfield
+# cvs checkout intranet-earned-value-management
+cvs checkout intranet-exchange-rate
+cvs checkout intranet-expenses
+cvs checkout intranet-expenses-workflow
+cvs checkout intranet-filestorage
+cvs checkout intranet-filestorage-openacs
+cvs checkout intranet-forum
+cvs checkout intranet-freelance
+# cvs checkout intranet-freelance-invoices
+# cvs checkout intranet-freelance-invoices-moravia	# (customer package)
+# cvs checkout intranet-freelance-rfqs
+# cvs checkout intranet-freelance-translation
+# cvs checkout intranet-funambol
+cvs checkout intranet-ganttproject
+# cvs checkout intranet-gtd-dashboard			# (work in progress)
+cvs checkout intranet-helpdesk
+cvs checkout intranet-hr
+# cvs checkout intranet-html2pdf			# (obsolete)
+cvs checkout intranet-idea-management
+cvs checkout intranet-invoices
+cvs checkout intranet-invoices-templates
+cvs checkout intranet-mail-import
+cvs checkout intranet-material
+cvs checkout intranet-milestone
+cvs checkout intranet-nagios
+cvs checkout intranet-notes
+cvs checkout intranet-notes-tutorial
+# cvs checkout intranet-ophelia
+# cvs checkout intranet-otp
+# cvs checkout intranet-otrs-integration		# (work in progress)
+cvs checkout intranet-payments
+# cvs checkout intranet-planning
+# cvs checkout intranet-procedures			# (obsolete)
+# cvs checkout intranet-pdf-htmldoc
+cvs checkout intranet-portfolio-management
+cvs checkout intranet-release-mgmt
+cvs checkout intranet-reporting
+# cvs checkout intranet-reporting-cubes
+cvs checkout intranet-reporting-dashboard
+# cvs checkout intranet-reporting-finance
+cvs checkout intranet-reporting-indicators
+cvs checkout intranet-reporting-openoffice
+# cvs checkout intranet-reporting-translation
+cvs checkout intranet-reporting-tutorial
+cvs checkout intranet-resource-management
+cvs checkout intranet-rest
+# cvs checkout intranet-rss-reader			# (internal)
+cvs checkout intranet-riskmanagement
+# cvs checkout intranet-scrum				# (work in progress)
+cvs checkout intranet-search-pg
+cvs checkout intranet-search-pg-files
+cvs checkout intranet-security-update-client
+# cvs checkout intranet-security-update-server
+# cvs checkout intranet-sencha				# (GPL V3.0 license)
+cvs checkout intranet-simple-survey
+# cvs checkout intranet-sla-management
+cvs checkout intranet-sharepoint
+# cvs checkout intranet-soap-lite-server		# (work in progress)
+# cvs checkout intranet-spam
+cvs checkout intranet-sql-selectors
+cvs checkout intranet-sysconfig
+cvs checkout intranet-timesheet2
+cvs checkout intranet-timesheet2-invoices
+# cvs checkout intranet-timesheet2-task-popup
+cvs checkout intranet-timesheet2-tasks
+cvs checkout intranet-timesheet2-workflow
+# cvs checkout intranet-tinytm
+# cvs checkout intranet-trans-invoices
+# cvs checkout intranet-trans-invoice-authorization	# (work in progress)
+# cvs checkout intranet-trans-project-wizard
+# cvs checkout intranet-trans-invoices-vaw		# (customer package)
+# cvs checkout intranet-translation
+# cvs checkout intranet-trans-quality
+cvs checkout intranet-ubl
+cvs checkout intranet-update-client
+# cvs checkout intranet-update-server
+cvs checkout intranet-wiki
+cvs checkout intranet-workflow
+# cvs checkout intranet-xmlrpc
+# cvs checkout lars-blogger				# (obsolete)
+cvs checkout mail-tracking
+cvs checkout notifications
+cvs checkout oacs-dav
+cvs checkout openacs-default-theme
+# cvs checkout organizations
+# cvs checkout oryx-ts-extensions
+# cvs checkout postal-address
+# cvs checkout ref-countries
+# cvs checkout ref-itu					# (obsolete)
+cvs checkout ref-language
+cvs checkout ref-timezones
+# cvs checkout ref-us-counties
+# cvs checkout ref-us-states
+# cvs checkout ref-us-zipcodes
+# cvs checkout rss-support
+# cvs checkout search
+cvs checkout simple-survey
+# cvs checkout telecom-number				# (obsolete? OpenACS package)
+# cvs checkout trackback				# (obsolete? OpenACS package)
+# cvs checkout tsearch2-driver				# (obsolete OpenACS package)
+# cvs checkout wiki
+cvs checkout workflow
+# cvs checkout xml-rpc
+cvs checkout xotcl-core
+cvs checkout xowiki
+
+# cvs checkout po-timesheet				# (Java REST Timesheet client)
+# cvs checkout tclwebtest				# (TCL client for performance testing)
+
