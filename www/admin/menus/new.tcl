448cc9027b908039647f14aebebf309104fbae73
Frank Bergmann <frank.bergmann@project-open.com>
2016-03-10 11:22:28 +0000

    - Return_url is now an optional parameter in order to make   it easier to link to this page

--- www/admin/menus/new.tcl
+++ www/admin/menus/new.tcl
@@ -13,7 +13,7 @@ ad_page_contract {
     @author frank.bergmann@project-open.com
 } {
     menu_id:integer,optional
-    return_url
+    { return_url "/intranet" }
     edit_p:optional
     message:optional
     { form_mode "edit" }
