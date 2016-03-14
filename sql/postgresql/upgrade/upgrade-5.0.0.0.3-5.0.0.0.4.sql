28407991cb0c62a14f620f792d5d6c45dc0596a9
Frank Bergmann <frank.bergmann@project-open.com>
2016-03-14 08:36:33 +0000

    -- added missing return

--- sql/postgresql/upgrade/upgrade-5.0.0.0.3-5.0.0.0.4.sql
+++ sql/postgresql/upgrade/upgrade-5.0.0.0.3-5.0.0.0.4.sql
@@ -3,9 +3,6 @@
 SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-5.0.0.0.3-5.0.0.0.4.sql','');
 
 
-
-
-
 -- Add a new constraint to avoid projects that end before they start.
 create or replace function inline_0()
 returns integer as $body$
@@ -18,6 +15,8 @@ BEGIN
 
 	update im_projects set end_date = start_date where end_date < start_date;
 	alter table im_projects add constraint im_projects_start_end_chk check(end_date >= start_date);
+	
+	return 0;
 
 END;$body$ language 'plpgsql';
 select inline_0();
