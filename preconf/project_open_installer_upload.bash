76ddf3b0c9665b21e21783732e7d72a3a2ed2a02
Frank Bergmann <frank.bergmann@project-open.com>
2017-04-10 10:31:23 +0000

    - Moved Windows stuff to ../windows-installer

--- preconf/project_open_installer_upload.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BASE=`echo "$1" | xargs basename`
-
-# echo 'rsync -avP -e ssh "$1" "fraber,project-open\@frs.sourceforge.net:/home/frs/project/p/pr/project-open/project-open/V5.0/"'
-
-echo rsync -avP -e ssh /cygdrive/c/download/$BASE "fraber,project-open\@frs.sourceforge.net:/home/frs/project/p/pr/project-open/project-open/V5.0/"
-
-
-exit 0
