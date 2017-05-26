1cd1f5bd11e929260805ba27f9e786b825d619f4
Frank Bergmann <frank.bergmann@project-open.com>
2017-05-26 10:32:34 +0000

    - Deleting im_biz_object_members:   Now added code to delete dependencies

--- sql/postgresql/intranet-biz-objects.sql
+++ sql/postgresql/intranet-biz-objects.sql
@@ -698,12 +698,23 @@ DECLARE
 	p_user_id	alias for $2;
 
 	v_rel_id	integer;
+	v_skill_profile_rel_id_exists_p	integer;
 BEGIN
 	select	rel_id
 	into	v_rel_id
 	from	acs_rels
 	where	object_id_one = p_object_id
 		and object_id_two = p_user_id;
+
+	-- Other rels can reference a rel...
+	select count(*) into v_skill_profile_rel_id_exists_p
+	from information_schema.columns 
+	where table_name = 'im_biz_object_members' and column_name = 'skill_profile_rel_id';
+	IF v_skill_profile_rel_id_exists_p > 0 THEN 
+		update im_biz_object_members
+		set skill_profile_rel_id = null
+		where skill_profile_rel_id = v_rel_id;			   
+	END IF;
 
 	delete	from im_biz_object_members
 	where	object_role_id = v_rel_id;
