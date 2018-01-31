b5da44f2285c4fa2a7c2529ae889d016a0888521
cosine <>
2018-01-31 14:59:56 +0000

    - Fixed issue with empty user

--- tcl/intranet-design-procs.tcl
+++ tcl/intranet-design-procs.tcl
@@ -2228,6 +2228,7 @@ ad_proc -public im_skin_select_html {
     user_id 
     return_url 
 } {
+    if {"" eq $user_id} { return "" }
     if {!$user_id} { return "" }
     if {![string is integer $user_id]} { im_security_alert -location "im_skin_select_html" -message "user_is is not an integer" -value $user_id -severity "Normal" }
 
