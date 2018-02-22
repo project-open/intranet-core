57f127ab990b0f955c4d8b2e8b5ee63bd033dae4
Frank Bergmann <frank.bergmann@project-open.com>
2018-02-22 18:32:51 +0000

    - Added function im_audit_baseline_value

--- sql/postgresql/intranet-audit-create.sql
+++ sql/postgresql/intranet-audit-create.sql
@@ -128,3 +128,34 @@ BEGIN
 	return v_result;
 end; $body$ language 'plpgsql';
 
+
+
+
+
+create or replace function im_audit_baseline_value (integer, text, integer)
+returns text as $body$
+DECLARE
+	p_object_id	alias for $1;
+	p_var_name	alias for $2;
+	p_baseline_id	alias for $3;
+
+	v_audit_value	text;
+	v_expr		text;
+	v_result	text;
+BEGIN
+	select	a.audit_value into v_audit_value
+	from	im_audits a
+	where	a.audit_id = (
+			select	min(aa.audit_id)
+			from	im_audits aa
+			where	aa.audit_object_id = p_object_id and
+				aa.audit_baseline_id = p_baseline_id
+		);
+
+	v_expr := p_var_name || '\t([^\n]*)';
+	select	substring(v_audit_value from v_expr) into v_result from dual;
+	IF '' = v_result THEN v_result := null; END IF;
+
+	return v_result;
+end; $body$ language 'plpgsql';
+
