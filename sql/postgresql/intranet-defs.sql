d272715bc97f3b85f966bc79b93d838868a89a38
cosine <>
2018-11-28 13:41:55 +0000

    - Added im_month_iterator function

--- sql/postgresql/intranet-defs.sql
+++ sql/postgresql/intranet-defs.sql
@@ -270,6 +270,29 @@ BEGIN
 end;$body$ language 'plpgsql';
 
 
+
+create or replace function im_month_enumerator (date, date) 
+returns setof date as $body$
+declare
+	p_start_date		alias for $1;
+	p_end_date		alias for $2;
+	row			RECORD;
+BEGIN
+	FOR row IN 
+	    	select distinct
+			to_char(im_day_enumerator, 'YYYY-MM')||'-01' as month
+		from
+			im_day_enumerator(p_start_date, p_end_date)
+		order by month
+	LOOP
+		RETURN NEXT row.month;
+	END LOOP;
+	RETURN;
+end;$body$ language 'plpgsql';
+
+
+
+
 create or replace function im_day_enumerator_weekdays (date, date) 
 returns setof date as $body$
 declare
