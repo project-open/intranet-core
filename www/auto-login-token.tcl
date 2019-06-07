647ff4653d35484e17c7c10adac91cdf52b17775
Frank Bergmann <frank.bergmann@project-open.com>
2019-06-07 11:11:14 +0000

    - Fixed text/json to application/json

--- www/auto-login-token.tcl
+++ www/auto-login-token.tcl
@@ -16,7 +16,7 @@ ad_page_contract {
 
 
 if {"" == $password || "" == $email} {
-    doc_return 200 "text/json" "FAIL - no email or password"
+    doc_return 200 "application/json" "FAIL - no email or password"
     ad_script_abort
 }
 
@@ -34,11 +34,11 @@ if {[info exists result_array(user_id)]} { set user_id $result_array(user_id) }
 if {"ok" == $account_status && 0 != $user_id} { 
     ad_user_login -forever=0 $user_id
     set token [im_generate_auto_login -user_id $user_id -expiry_date $expiry_date]
-    doc_return 200 "text/json" "{\"success\": true, \"message\": \"success\", \"token\": \"$token\"}"
+    doc_return 200 "application/json" "{\"success\": true, \"message\": \"success\", \"token\": \"$token\"}"
     ad_script_abort
 } else {
     set auth_message $result_array(auth_message)
-    doc_return 401 "text/json" "{\"success\": false, \"message\": \"$auth_message\"}"
+    doc_return 401 "application/json" "{\"success\": false, \"message\": \"$auth_message\"}"
     ad_script_abort
 }
 
