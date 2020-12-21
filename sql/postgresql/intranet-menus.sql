f06899047cb01407acb46b98a369e299d63220f1
Frank Bergmann <frank.bergmann@project-open.com>
2020-12-21 20:58:55 +0100

    - Added a new menu for project_admin

--- sql/postgresql/intranet-menus.sql
+++ sql/postgresql/intranet-menus.sql
@@ -1103,20 +1103,12 @@ declare
 	v_admin_menu		integer;
 	v_main_menu		integer;
 BEGIN
-	select menu_id
-	into v_main_menu
-	from im_menus
-	where label = 'projects';
+	select menu_id into v_main_menu from im_menus where label = 'projects';
 
 	-- Main admin menu - just an invisible top-menu
 	-- for all admin entries links under Projects
 	v_admin_menu := im_menu__new (
-		null,			-- p_menu_id
-		'im_menu',		-- object_type
-		now(),			-- creation_date
-		null,			-- creation_user
-		null,			-- creation_ip
-		null,			-- context_id
+		null, 'im_menu', now(), null, null, null,
 		'intranet-core',	-- package_name
 		'projects_admin',	-- label
 		'Projects Admin',	-- name
@@ -1130,6 +1122,39 @@ BEGIN
 end;$body$ language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
+
+
+
+create or replace function inline_0 ()
+returns integer as $body$
+declare
+	-- Menu IDs
+	v_menu			integer;
+	v_admin_menu		integer;
+	v_main_menu		integer;
+BEGIN
+	select menu_id into v_main_menu from im_menus where label = 'projects';
+
+	-- Main admin menu - just an invisible top-menu
+	-- for all admin entries links under Projects
+	v_admin_menu := im_menu__new (
+		null, 'im_menu', now(), null, null, null,
+		'intranet-core',	-- package_name
+		'project_admin',	-- label
+		'Project Admin',	-- name
+		'/intranet-core/',	-- url
+		95,			-- sort_order
+		v_main_menu,		-- parent_menu_id
+		'0'			-- p_visible_tcl
+	);
+
+	return 0;
+end;$body$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
 
 
 
