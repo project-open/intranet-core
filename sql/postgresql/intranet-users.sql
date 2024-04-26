d092fc59352777071bed7bef694a01b433b5bc69
Frank Bergmann <frank.bergmann@project-open.com>
2024-04-26 09:25:58 +0200

    cosine #5704: Added persons.initials

--- sql/postgresql/intranet-users.sql
+++ sql/postgresql/intranet-users.sql
@@ -26,12 +26,12 @@ alter table persons add demo_group varchar(50);
 alter table persons add demo_password varchar(50);
 
 
+
 -------------------------------------------------------------
 -- Skin Field
 --
 
 alter table users add skin_id integer default 0;
-
 
 -------------------------------------------------------------
 -- Tell OpenACS that users and persons are tables for
@@ -304,4 +304,73 @@ select
 from 	im_categories
 where	category_type = 'Intranet User Status'
 	and (enabled_p is null OR enabled_p = 't');
+
+
+
+-------------------------------------------------------------
+-- Initials for Gantt Editor
+--
+alter table persons drop if exists initials;
+alter table persons add initials varchar;
+
+
+-------------------------------------------------------------------
+-- Trigger for initials
+-------------------------------------------------------------------
+
+drop trigger if exists persons_initials_default_update_tr ON persons;
+drop function if exists persons_initials_default_update_tr();
+
+create or replace function persons_initials_default_update_tr () 
+returns trigger as $body$
+declare
+	v_name		varchar;
+	v_initials	varchar;
+	v_state		integer;
+	v_i		integer;
+	v_char		varchar;
+begin
+	-- Ignore if initials are already set
+	IF old.initials is not null THEN return new; END IF;
+
+	-- We update persons below, so avoid loops
+	IF pg_trigger_depth() > 1 THEN return new; END IF;
+
+	SELECT upper(regexp_replace(new.first_names || ' ' || new.last_name, '\W', ' ', 'g')) INTO v_name;
+
+	-- Mini state-engine looking at letter-space and space-letter transitions
+	v_initials := '';
+	v_state := 0; -- 0=space, 1=letters
+	FOR v_i IN 1..length(v_name) LOOP
+		v_char := substring(v_name from v_i for 1);
+		IF v_state = 0 THEN
+			-- We had spaces before   
+			IF ' ' = v_char THEN
+				-- more spaces, do nothing
+			ELSE
+				-- found a first char
+				v_initials := v_initials || v_char;
+				v_state := 1;
+			END IF;
+		ELSE
+			-- We had chars before
+			IF ' ' = v_char THEN
+				-- found a first space after chars
+				v_state := 0;
+			ELSE
+				-- more chars, do nothing
+			END IF;	
+		END IF;
+	END LOOP;
+	update persons set initials = v_initials where person_id = new.person_id;
+	-- RAISE NOTICE 'persons_initials_default_update_tr: found v_name=%, v_initials=%', v_name, v_initials;
+
+	return new;
+end;$body$ language 'plpgsql';
+
+create trigger persons_initials_default_update_tr after update
+on persons for each row execute procedure persons_initials_default_update_tr ();
+
+-- update persons set person_id = person_id where person_id < 10000;
+-- select * from persons where initials is not null;
 
---
+++ sql/postgresql/upgrade/upgrade-5.1.0.0.0-5.1.0.0.1.sql
@@ -0,0 +1,70 @@
+SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-5.1.0.0.0-5.1.0.0.1.sql','');
+
+
+-------------------------------------------------------------
+-- Initials for Gantt Editor
+--
+alter table persons drop if exists initials;
+alter table persons add initials varchar;
+
+
+-------------------------------------------------------------------
+-- Trigger for initials
+-------------------------------------------------------------------
+
+drop trigger if exists persons_initials_default_update_tr ON persons;
+drop function if exists persons_initials_default_update_tr();
+
+create or replace function persons_initials_default_update_tr () 
+returns trigger as $body$
+declare
+	v_name		varchar;
+	v_initials	varchar;
+	v_state		integer;
+	v_i		integer;
+	v_char		varchar;
+begin
+	-- Ignore if initials are already set
+	IF old.initials is not null THEN return new; END IF;
+
+	-- We update persons below, so avoid loops
+	IF pg_trigger_depth() > 1 THEN return new; END IF;
+
+	SELECT upper(regexp_replace(new.first_names || ' ' || new.last_name, '\W', ' ', 'g')) INTO v_name;
+
+	-- Mini state-engine looking at letter-space and space-letter transitions
+	v_initials := '';
+	v_state := 0; -- 0=space, 1=letters
+	FOR v_i IN 1..length(v_name) LOOP
+		v_char := substring(v_name from v_i for 1);
+		IF v_state = 0 THEN
+			-- We had spaces before   
+			IF ' ' = v_char THEN
+				-- more spaces, do nothing
+			ELSE
+				-- found a first char
+				v_initials := v_initials || v_char;
+				v_state := 1;
+			END IF;
+		ELSE
+			-- We had chars before
+			IF ' ' = v_char THEN
+				-- found a first space after chars
+				v_state := 0;
+			ELSE
+				-- more chars, do nothing
+			END IF;	
+		END IF;
+	END LOOP;
+	update persons set initials = v_initials where person_id = new.person_id;
+	-- RAISE NOTICE 'persons_initials_default_update_tr: found v_name=%, v_initials=%', v_name, v_initials;
+
+	return new;
+end;$body$ language 'plpgsql';
+
+create trigger persons_initials_default_update_tr after update
+on persons for each row execute procedure persons_initials_default_update_tr ();
+
+-- update persons set person_id = person_id where person_id < 10000;
+-- select * from persons where initials is not null;
+
