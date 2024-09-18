f5a995e12a7359711b650f72d5b5680c20103d80
Frank Bergmann <frank.bergmann@project-open.com>
2024-09-18 11:29:19 +0200

    cosine #5962: Showing audit "view" events as errors

--- sql/postgresql/upgrade/upgrade-5.1.0.0.1-5.1.0.0.2.sql
+++ sql/postgresql/upgrade/upgrade-5.1.0.0.1-5.1.0.0.2.sql
@@ -4,3 +4,17 @@ alter table im_audits drop constraint im_audits_action_ck;
 alter table im_audits add constraint im_audits_action_ck 
       check (audit_action in ('after_create','before_update','after_update','before_nuke', 'view', 'view_fixed', 'baseline'));
 
+
+/*
+
+set invoice_ids [db_list invoice_ids "select invoice_id from im_invoices"]
+foreach id $invoice_ids {
+  append debug "\n$id"
+  im_audit -object_id $id -action "view"
+}
+set debug
+
+*/
+
+
+update im_audits set audit_action = 'view_fixed' where audit_action = 'view';
